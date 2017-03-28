5280f5a74018247bf11e95447dfe2b589d904eb8
Danny Mandel <Danny Mandel>
2017-03-28 17:54:50 -0400

    First commit with login page created

---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('starter.controllers', [])
+
+.controller('noEmptyLegsCtrl', function($scope) {
+
+})
+
+.controller('LoginCtrl', function($scope, $stateParams) {
+  console.log("Hello World")
+});
