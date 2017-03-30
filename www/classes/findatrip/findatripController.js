a1707d52ea0edb61cd75f3e2cb43e492620e11c4
Danny Mandel <Danny Mandel>
2017-03-30 17:39:31 -0400

    added date selector and toggle with logic

--- www/classes/findatrip/findatripController.js
+++ www/classes/findatrip/findatripController.js
@@ -1,5 +1,16 @@
 angular.module('findatrip.controller', [])
 
 .controller('FindCtrl', function($scope, $stateParams, $rootScope, $location) {
- 
+
+
+
+  $scope.searchByDate = false;
+  $scope.departureDate = new Date();
+  $scope.returnDate = new Date();
+
+  $scope.toggleDateSelector = function (){
+    $scope.searchByDate = !$scope.searchByDate;
+  }
+
+
 });
