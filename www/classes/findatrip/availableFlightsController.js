3f4942481cdb42ffee3838754df771344f780222
Daniel Mandel <mandeldr@mcmaster.ca>
2017-04-03 14:37:08 -0400

    added a confirm dialog box upon clicking book me

--- www/classes/findatrip/availableFlightsController.js
+++ www/classes/findatrip/availableFlightsController.js
@@ -1,7 +1,25 @@
 angular.module('availableflights.controller', [])
 
-.controller('AvailCtrl', function($scope, $stateParams, $rootScope, $location, $http, $timeout, $q, $log, flightFilter) {
+.controller('AvailCtrl', function($scope, $stateParams, $rootScope, $location, $http, $timeout, $q, $log, $mdDialog, flightFilter) {
 
   $scope.flights = flightFilter.getAvilableFlights();
+
+  $scope.book = function(ev){
+    console.log(ev);
+    var selectedFlight = $scope.flights[ev];
+    var confirm = $mdDialog.confirm()
+          .title('Please confirm your booking')
+          .htmlContent('')
+          .ariaLabel('')
+          .targetEvent(ev)
+          .ok('Confirm')
+          .cancel('Cancel');
+          $mdDialog.show(confirm).then(function() {
+    $scope.status = 'You decided to get rid of your debt.';
+  }, function() {
+    $scope.status = 'You decided to keep your debt.';
+  });
+
+  }
 
 });
