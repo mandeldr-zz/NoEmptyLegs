5110982a88ff1495643079e10e43ac650ef64ca6
Daniel Mandel <mandeldr@mcmaster.ca>
2017-04-04 00:38:39 -0400

    cleaned up html on create account page, extended user service

--- www/js/userService.js
+++ www/js/userService.js
@@ -2,31 +2,26 @@ angular.module('user.services', [])
 
 .service('userService', function($location, $mdDialog) {
 
-  var userList = [];
+    var userList = [];
 
-  var owner = {id: '01', username: 'owner1', password: 'owner1', userType: 'owner'};
-  //customer
-  var customer = {id: '02', username: 'customer1', password: 'customer1', userType: 'customer'};
-  //crew
-  var crew = {id: '03', username: 'crew1', password: 'crew1', userType: 'crew'};
+    var owner = {id: '01', username: 'owner1', password: 'owner1', userType: 'owner'};
+    //customer
+    var customer = {id: '02', username: 'customer1', password: 'customer1', userType: 'customer'};
+    //crew
+    var crew = {id: '03', username: 'crew1', password: 'crew1', userType: 'crew'};
 
     userList.push(owner);
     userList.push(customer);
     userList.push(crew);
+
+    this.user = null;
 
     this.login = function(username, password){
       for (var i in userList){
         try{
           if(userList[i].username == username && userList[i].password == password){
             console.log("Authenticated");
-            //assign credentials to the logged in user
-            //store on root scope for all controllers
-            //check if rememberMe is selected
-            //if ($scope.rememberMe == false) {
-              //$scope.user.username = '';
-              //$scope.user.password = '';
-            //}
-            //$location.path('/home');
+            this.setUser(userList[i]);
             return true;
           }
         }
@@ -61,6 +56,14 @@ angular.module('user.services', [])
 
     this.signOut = function(){
 
+    }
+
+    this.setUser = function(user){
+      this.user = user;
+    }
+
+    this.getUser = function(){
+      return this.user;
     }
 
 });
